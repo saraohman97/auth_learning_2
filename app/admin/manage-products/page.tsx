c937bc591bbe637998faad83fc359885dc405b95
Sara Öhman <saraohman97@gmail.com>
2024-01-25 15:35:33 +0100

    Get products action.

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,6 +1,18 @@
 
-const ManageProducts = () => {
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import getProducts from "@/actions/getProducts";
+import NullData from "@/components/null-data";
+
+const ManageProducts = async () => {
+    const products = await getProducts({category: null})
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser || currentUser.role !== 'ADMIN') {
+        return <NullData title="Ops! Access denied!" />
+    }
+
     return ( 
+        
         <div>products</div>
      );
 }
