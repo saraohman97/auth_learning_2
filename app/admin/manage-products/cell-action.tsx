e3aec6236f3cdffc7dd8aec0832e44d4e5aa7e72
Sara Öhman <saraohman97@gmail.com>
2024-01-30 11:28:39 +0100

    Data table action delete.

---
+++ app/admin/manage-products/cell-action.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import {TableProduct} from './columns'
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useCallback } from "react";
+import { toast } from "react-toastify";
+import { deleteObject, getStorage, ref } from "firebase/storage";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import firebaseApp from '@/lib/firebase'
+
+interface CellActionProps {
+    data: TableProduct;
+}
+
+const CellAction: React.FC<CellActionProps> = ({ data }) => {
+    const storage = getStorage(firebaseApp);
+    const router = useRouter();
+  
+    const handleDelete = useCallback(async (id: string) => {
+      toast("Deleting product, please wait!");
+  
+    //   const handleImageDelete = async () => {
+    //     try {
+    //       for (const item of images) {
+    //         if (item.image) {
+    //           const imageRef = ref(storage, item.image);
+    //           await deleteObject(imageRef);
+    //           console.log("Image deleted", item.image);
+    //         }
+    //       }
+    //     } catch (error) {
+    //       return console.log("Deleting images error", error);
+    //     }
+    //   };
+    //   await handleImageDelete();
+  
+      axios.delete(`/api/product/${id}`)
+      .then((res) => {
+        toast.success("Product status changed");
+        router.refresh;
+      })
+      .catch ((error) => {
+          toast.error('Failed to delete product')
+          console.log(error)
+      })
+    }, [router.refresh, storage]);
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(data.id)}
+        >
+          Copy product ID
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => handleDelete(data.id)}>Delete product</DropdownMenuItem>
+        <DropdownMenuItem>Edit product</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+ 
+export default CellAction;
--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,80 +1,21 @@
-// import { getCurrentUser } from "@/actions/getCurrentUser";
-// import getProducts from "@/actions/getProducts";
-// import NullData from "@/components/null-data";
-// import { Button } from "@/components/ui/button";
-// import Image from "next/image";
-// import { FaEdit, FaTrash } from "react-icons/fa";
-
-// const ManageProducts = async () => {
-//   const products = await getProducts({ category: null });
-//   const currentUser = await getCurrentUser();
-
-//   if (!currentUser || currentUser.role !== "ADMIN") {
-//     return <NullData title="Ops! Access denied!" />;
-//   }
-
-//   return (
-//     <div className="mx-auto max-w-screen-sm border mt-20">
-//       <div className="bg-slate-100 grid grid-cols-3">
-//         <div className="w-full text-center">Name</div>
-//         <div className="w-full text-center">Image</div>
-//         <div className="w-full text-center">Actions</div>
-//       </div>
-//       {products.map((product) => (
-//         <div key={product.id} className="grid grid-cols-3">
-//           <div className="border-r border-t w-full flex items-center justify-center">
-//             {product.name}
-//           </div>
-//           <div className="border-t text-center flex items-center justify-center relative min-h-20">
-//             <Image
-//               src={product.images[0].image}
-//               alt=""
-//               fill
-//               className="object-cover absolute"
-//             />
-//           </div>
-//           <div className="flex items-center justify-center gap-2">
-//             <Button>
-//               <FaTrash />
-//             </Button>
-//             <Button>
-//               <FaEdit />
-//             </Button>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default ManageProducts;
-
 import getProducts from "@/actions/getProducts";
-import { TableProduct, columns } from "./columns"
-import { DataTable } from "./data-table"
+import { TableProduct, columns } from "./columns";
+import { DataTable } from "./data-table";
 
 async function getData(): Promise<TableProduct[]> {
   // Fetch data from your API here.
   const products = await getProducts({ category: null });
-  
+
   return products;
-//   [
-//     {
-//       id: products.id,
-//       name: "",
-//       inStock: "yes",
-//       category: "m@example.com",
-//     },
-//   ]
 }
 
 export default async function DemoPage() {
-  const data = await getData()
+    const data = await getData();
 
   return (
     <div className="max-w-screen-lg mx-auto py-10">
-        <h2 className="text-xl text-center mb-4">Manage products</h2>
+      <h2 className="text-xl text-center mb-4">Manage products</h2>
       <DataTable columns={columns} data={data} />
     </div>
-  )
+  );
 }
